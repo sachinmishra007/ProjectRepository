3503a3761558c88d5dbc2b4c519f85c910cbe17e
sachinmishra007 <sachinmishra609@gmail.com>
2017-11-19 18:12:46 +0530

    Modified Code

--- MVCProjectExample.UI/MVCProjectExample.UI/Scripts/app.js
+++ MVCProjectExample.UI/MVCProjectExample.UI/Scripts/app.js
@@ -30,17 +30,49 @@
             transclude: false,
             require: 'ngModel',
             link: function (scope, element, attrs, controller) {
-                controller.$parsers.push(function (inputValue) {
-                    var transformedInput;
-                    if (isNaN(inputValue) || inputValue == null || inputValue < 0) {
-                        transformedInput = 0;
+                //controller.$parsers.push(function (inputValue) {
+                //    var transformedInput;
+                //    if (isNaN(inputValue) || inputValue == null || inputValue < 0) {
+                //        transformedInput = 0;
+                //    }
+                //    if (transformedInput != inputValue) {
+                //        controller.$setViewValue(transformedInput);
+                //        controller.$render();
+                //    }
+                //    return transformedInput;
+                //});
+
+                scope.AddNumbertoBucket = function () {
+                    scope.userdetails.ItemCount += 1;
+                };
+
+                element.on('keydown', function (event) {
+                    var $input = $(this);
+                    var value = $input.val();
+
+                    value = value.replace(/[^0-9]+\.?[0-9]*$/g, '')
+                    $input.val(value);
+                    if (event.which == 64 || event.which == 16) {
+                        // to allow numbers  
+                        return false;
+                    } else if (event.which >= 48 && event.which <= 57) {
+                        // to allow numbers  
+                        return true;
+                    } else if (event.which >= 96 && event.which <= 105) {
+                        // to allow numpad number  
+                        return true;
+                    } else if ([8, 13, 27, 37, 38, 39, 40, 46].indexOf(event.which) > -1) {
+                        // to allow backspace, enter, escape, arrows  
+                        return true;
+                    } else {
+                        event.preventDefault();
+                        // to stop others  
+                        //alert("Sorry Only Numbers Allowed");  
+                        return false;
                     }
-                    if (transformedInput != inputValue) {
-                        controller.$setViewValue(transformedInput);
-                        controller.$render();
-                    }
-                    return transformedInput;
+
                 });
+
             }
         }
     })
